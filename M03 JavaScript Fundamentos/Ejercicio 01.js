7d3b3969d3840d57b2d44e5ab68c5b14cadfd62f
Facundo Gamon <101083181+facugamon@users.noreply.github.com>
2023-10-02 00:26:41 -0300

    Update Ejercicio 01.js

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,22 +5,22 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+var nuevoString = "Este es una variable string;
 
 // Crea una variable de tipo number.
-const nuevoNumero = null;
+var nuevoNumero = 1;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = null;
+var nuevoBoolean = true;
 
 // Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - null === 3;
+var nuevaResta = 10 - 7 === 3;
 
 // Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * null === 40;
+var nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático.
-const nuevoModulo = 21 % 5 === null;
+var nuevoModulo = 21 % 5 === 4;
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
